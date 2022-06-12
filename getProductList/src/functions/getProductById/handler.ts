a47f741676c185d6d3ccd7d322f307ce28c5ace9
mykamapolice <ilya.barachenia@stxnext.pl>
2022-06-12 22:36:46 +0200

    feat: implement handler and data

--- getProductList/src/functions/getProductById/handler.ts
+++ getProductList/src/functions/getProductById/handler.ts
@@ -7,7 +7,7 @@ const data = require('../../data.json');
 const getProductById: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
   const {productId} = event.pathParameters
 
-  const item = JSON.parse(JSON.stringify(data)).find(item => Number(item.id) === Number(productId))
+  const item = await JSON.parse(JSON.stringify(data)).find(item => Number(item.id) === Number(productId))
   return formatJSONResponse({
     item,
   });
