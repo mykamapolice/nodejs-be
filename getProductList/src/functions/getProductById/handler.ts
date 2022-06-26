28d05c842c4ff05d931e01c06291ea0dabd031b8
mykamapolice <ilya.barachenia@stxnext.pl>
2022-06-26 20:00:19 +0200

    add log

--- getProductList/src/functions/getProductById/handler.ts
+++ getProductList/src/functions/getProductById/handler.ts
@@ -6,6 +6,7 @@ const data = require('../../data.json');
 
 const getProductById: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
   const {productId} = event.pathParameters
+  console.log(productId)
 
   const item = await JSON.parse(JSON.stringify(data)).find(item => Number(item.id) === Number(productId))
   return formatJSONResponse({
